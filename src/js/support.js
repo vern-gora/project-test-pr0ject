36f557ae1272b2772261399e4f3b856b2f6eda41
Yulia-Kot <yuhalaniuk@ukr.net>
2023-07-01 01:05:36 +0300

    updated support.js

--- src/js/support.js
+++ src/js/support.js
@@ -25,9 +25,9 @@
 
 // //   ---------- Version with replacing atrributes ----------
 
-// import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, { Navigation, Pagination } from 'swiper';
 
-// Swiper.use([Navigation, Pagination]);
+Swiper.use([Navigation, Pagination]);
 
 const charityArray = [
   {
@@ -77,113 +77,11 @@ const charityArray = [
   },
 ];
 
-// const charityLinks = Array.from(document.querySelectorAll('.charity-link'));
-
-// let i = 0;
-
-// const hey = charityLinks.map(org => {
-//   org.href = charityArray[i].url;
-//   org.querySelector('img').alt = charityArray[i].title;
-//   i += 1;
-// });
-
-// // --------------Swiper ------------
-
-// const swiper = new Swiper('.swiper', {
-//   direction: 'vertical',
-
-//   width: 255,
-//   height: 236,
-
-//   spaceBetween: 20,
-
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-
-//   observer: true,
-//   observeParents: true,
-//   slidesPerColumnFill: 'row',
-
-//   breakpoints: {
-//     375: {
-//       slidesPerView: 4,
-//     },
-
-//     768: {
-//       slidesPerView: 6,
-//     },
-
-//     1440: {
-//       slidesPerView: 6,
-//     },
-//   },
-// });
-
-// const upBtn = document.querySelector('.swiper-button-up');
-
-// upBtn.addEventListener('click', () => {
-//   swiper.slideTo(0, 800);
-//   upBtn.classList.add('is-hidden');
-// });
-
-// swiper.on('slideChange', () => {
-//   const activeSlideIndex = swiper.activeIndex;
-//   const totalSlides = swiper.slides.length;
-
-//   let neededNumber = totalSlides - getNumberOfScrolls();
-
-//   if (activeSlideIndex === neededNumber) {
-//     upBtn.classList.remove('is-hidden');
-//   }
-// });
-
-// window.addEventListener('resize', function () {
-//   swiper.params.slidesPerView = getSlidesPerView();
-
-//   swiper.update();
-// });
-
-// function getSlidesPerView() {
-//   const windowWidth = window.innerWidth;
-//   const breakpoints = swiper.params.breakpoints;
-
-//   if (windowWidth >= 1440 && breakpoints[1440]) {
-//     return breakpoints[1440].slidesPerView;
-//   } else if (windowWidth >= 768 && breakpoints[768]) {
-//     return breakpoints[768].slidesPerView;
-//   } else if (breakpoints[375]) {
-//     return breakpoints[375].slidesPerView;
-//   }
-
-//   return 6;
-// }
-
-// function getNumberOfScrolls() {
-//   const windowWidth = window.innerWidth;
-//   let currentSlidesPerView;
-
-//   if (windowWidth >= 1440) {
-//     currentSlidesPerView = swiper.params.breakpoints[1440].slidesPerView;
-//   } else if (windowWidth >= 780) {
-//     currentSlidesPerView = swiper.params.breakpoints[768].slidesPerView;
-//   } else {
-//     currentSlidesPerView = swiper.params.breakpoints[375].slidesPerView;
-//   }
-
-//   return currentSlidesPerView;
-// }
-
-import Swiper, { Navigation, Pagination } from 'swiper';
-
-Swiper.use([Navigation, Pagination]);
-
 const charityLinks = Array.from(document.querySelectorAll('.charity-link'));
 
 let i = 0;
 
-const hey = charityLinks.map(org => {
+charityLinks.map(org => {
   org.href = charityArray[i].url;
   org.querySelector('img').alt = charityArray[i].title;
   i += 1;
@@ -194,7 +92,6 @@ const hey = charityLinks.map(org => {
 const swiperOptions = {
   direction: 'vertical',
 
-  slidesPerView: 4,
   spaceBetween: 20,
 
   navigation: {
@@ -206,50 +103,42 @@ const swiperOptions = {
   observeParents: true,
   slidesPerColumnFill: 'row',
 
-  //   breakpoints: {
-  //     // when window width is >= 320px
+  breakpoints: {
+    320: {
+      slidesPerView: 4,
+    },
 
-  //     320: {
-  //       slidesPerView: 4,
-  //       height: 236,
-  //     },
-  //     // when window width is >= 480px
-  //     780: {
-  //       slidesPerView: 6,
-  //       height: 292,
-  //     },
+    780: {
+      slidesPerView: 6,
+    },
 
-  //     // when window width is >= 640px
-  //     //    1440: {
-  //     //       slidesPerView: 4,
-  //     //       height: 292,
-  //   },
+    1440: {
+      slidesPerView: 6,
+    },
+  },
 };
+
 const swiper = new Swiper('.swiper', swiperOptions);
 
 const upBtn = document.querySelector('.swiper-button-up');
-
-swiper.on('slideChange', () => {
-  const activeSlideIndex = swiper.activeIndex;
-  const totalSlides = swiper.slides.length;
-
-  //   let neededNumber = totalSlides - getNumberOfClickForSwiper();
-
-  //   if (activeSlideIndex === neededNumber) {
-  //     upBtn.classList.remove('is-hidden');
-  //   }
-
-  if (activeSlideIndex === swiperOptions.slidesPerView + 1) {
-    upBtn.classList.remove('is-hidden');
-  }
-});
 
 upBtn.addEventListener('click', () => {
   swiper.slideTo(0, 800);
   upBtn.classList.add('is-hidden');
 });
 
-function getNumberOfClickForSwiper() {
+swiper.on('slideChange', () => {
+  const activeSlideIndex = swiper.activeIndex;
+  const totalSlides = swiper.slides.length;
+
+  let slideNumberToGoUp = totalSlides - getNumberOfClicksForSwiper();
+
+  if (activeSlideIndex === slideNumberToGoUp) {
+    upBtn.classList.remove('is-hidden');
+  }
+});
+
+function getNumberOfClicksForSwiper() {
   const windowWidth = window.innerWidth;
   let currentSlidesPerView;
 
