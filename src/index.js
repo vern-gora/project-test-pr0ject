5aa857d683d3078a0e63fb61e9e02f3af102127b
vern-gora <103502360+vern-gora@users.noreply.github.com>
2023-06-30 14:13:32 +0300

    feature/setup

---
+++ src/index.js
@@ -0,0 +1,14 @@
+/**
+  |============================
+  | include js partials with
+  | default import
+  |============================
+*/
+import './js/header.js';
+import './js/menu.js';
+import './js/categories.js';
+import './js/support.js';
+import './js/books.js';
+import './js/modal.js';
+import './js/shopping-list.js';
+import './js/book-card.js';
