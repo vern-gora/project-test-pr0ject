d823f8e3d07ac527f5aad069a73128a45c2fb919
vladyslavbabenk0 <thevladman17@gmail.com>
2023-07-02 13:24:23 +0200

    scroll

---
+++ src/js/button-scroll.js
@@ -0,0 +1,17 @@
+window.onscroll = function() {
+    scrollFunction();
+  };
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("scrollButton").style.display = "block";
+    } else {
+      document.getElementById("scrollButton").style.display = "none";
+    }
+  }
+  
+  function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+  
