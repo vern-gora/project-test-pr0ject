69ab39a0a4988fd58f63ca91286c5672e87d4250
vladyslavbabenk0 <thevladman17@gmail.com>
2023-07-02 13:52:39 +0200

    scroll

--- src/js/button-scroll.js
+++ src/js/button-scroll.js
@@ -1,17 +1,30 @@
-window.onscroll = function() {
-    scrollFunction();
-  };
-  
-  function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      document.getElementById("scrollButton").style.display = "block";
-    } else {
-      document.getElementById("scrollButton").style.display = "none";
-    }
+const goTopBtn = document.querySelector(".go-top");
+
+
+window.addEventListener("scroll", trackScroll);
+
+goTopBtn.addEventListener("click", goTop);
+
+function trackScroll() {
+
+  const scrollY = window.pageYOffset;
+
+  const coords = document.documentElement.clientHeight;
+
+  if (scrollY > coords) {
+
+    goTopBtn.classList.add("go-top-show");
+  } else {
+
+    goTopBtn.classList.remove("go-top-show");
   }
+}
+
+function goTop() {
+
+  if (window.pageYOffset > 0) {
   
-  function scrollToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+    window.scrollBy(0, -25);
+    setTimeout(goTop, 0); 
   }
-  
+}
