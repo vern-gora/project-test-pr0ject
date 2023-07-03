da192747c4f5af0953ee2640a93f773c92c1e0c2
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2023-07-03 21:52:42 +0300

    firebase

--- src/index.js
+++ src/index.js
@@ -16,17 +16,17 @@ import './js/shopping-list.js';
 import './js/book-card.js';
 import './js/button-scroll.js';
 import './js/footer.js';
-import './js/firebase/firebase-api.js';
-import './js/firebase/firebase-config.js';
-import { getUserData, signOut } from './js/firebase/firebase-api.js';
+//import './js/firebase/firebase-api.js';
+//import './js/firebase/firebase-config.js';
+//import { getUserData, signOut } from './js/firebase/firebase-api.js';
 
-isUserSignIn();
+//isUserSignIn();
 
-async function isUserSignIn() {
-  const userId = localStorage.getItem('userId');
-  if (!userId) {
-    signOut();
-  } else {
-    getUserData();
-  }
-}
+// async function isUserSignIn() {
+//   const userId = localStorage.getItem('userId');
+//   if (!userId) {
+//     signOut();
+//   } else {
+//     getUserData();
+//   }
+// }
