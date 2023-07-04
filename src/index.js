5984d2eba1374a7314d8442fed1d80902124f91f
vern-gora <103502360+vern-gora@users.noreply.github.com>
2023-07-04 23:48:24 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@
 */
 import './js/header.js';
 import './js/menu.js';
-// import './js/categories.js';
-// import './js/support.js';
-// import './js/support-items.js';
-// import './js/support-img-array.js';
+import './js/categories.js';
+import './js/support.js';
+import './js/support-items.js';
+import './js/support-img-array.js';
 import './js/books.js';
 import './js/modal.js';
 import './js/shopping-list.js';
 import './js/book-card.js';
-// import './js/button-scroll.js';
+import './js/button-scroll.js';
 import './js/footer.js';
 import './js/api.js';
 import './js/firebase/firebase-api.js';
