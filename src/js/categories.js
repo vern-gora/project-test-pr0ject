b41a6fddf922fead902fbab8915863173b281673
vern-gora <103502360+vern-gora@users.noreply.github.com>
2023-07-04 14:15:53 +0300

    Update categories.js

--- src/js/categories.js
+++ src/js/categories.js
@@ -2,7 +2,7 @@ import { searchCategory } from './api.js';
 import { renderCategories, bookList } from './books.js';
 const apiUrl = 'https://books-backend.p.goit.global/books/category-list';
 const categoriesList = document.querySelector('.categories-list');
-const bookList = document.querySelector('.book-list');
+let bookList = document.querySelector('.book-list');
 
 function searchCategory(selectedCategory) {
   return fetch(
