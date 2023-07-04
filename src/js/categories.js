ebf66cc006a35e901bb332945edb141222a8ba77
iifeofadarkrose <122921193+iifeofadarkrose@users.noreply.github.com>
2023-07-05 00:01:02 +0200

    fixed categories + book

--- src/js/categories.js
+++ src/js/categories.js
@@ -33,6 +33,7 @@ function searchCategory(selectedCategory) {
       throw new Error('There is no such category');
     });
 }
+
 function setActiveCategory(selectedCategoryItem) {
   const activeCategoryItem = document.querySelector('.categories-el.active');
   if (activeCategoryItem) {
