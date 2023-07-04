2066a6f7601de539b6038045b6d3de8d733d9545
NovikovaMarina <xmarinovikova@gmail.com>
2023-07-04 17:00:47 +0300

    modal window fix

--- src/js/books.js
+++ src/js/books.js
@@ -157,6 +157,8 @@ function renderBooks(array) {
       );
     });
   });
+  console.log(markup);
+  return markup;
 }
 
 function renderCategories(array, container) {
@@ -183,15 +185,75 @@ function addToStorage() {
 
   books.forEach(book => {
     book.addEventListener('click', e => {
+
       const bookcard = e.currentTarget;
       const id = bookcard.dataset.id;
       const bookData = {
         id,
       };
-
+      
+       modalEl.classList.remove("is-hidden");
       if (e.target.tagName !== 'BUTTON') {
         localStorage.setItem('bookinfo', JSON.stringify(bookData));
       }
     });
   });
+
+  const modalOpenBtn = document.querySelectorAll('[data-action="open-modal"]');
+const modalCloseBtn = document.querySelector("[data-modal-cls]");
+const modalEl = document.querySelector("[data-modal-El]");
+const imageEl = document.querySelector(".main-img");
+const titleEl = document.querySelector(".modal-title");
+const authorEl = document.querySelector(".modal-author");
+const descriptionEl = document.querySelector(".modal-description");
+const addToListBtn = document.querySelector(".modal-btn");
+const textSubmitEl = document.querySelector(".modal-submit-text")
+
+modalOpenBtn.forEach(btn => 
+    btn.addEventListener("click", () => {
+        const savedBookId = localStorage.getItem("bookinfo");
+        const parsedBookId = JSON.parse(savedBookId);
+        const bookId = parsedBookId.id;
+        searchById(bookId).then(data => { 
+            imageEl.src = data.book_image;
+            authorEl.textContent = data.author;
+            titleEl.textContent = data.title;
+            descriptionEl.textContent = data.description;
+        })
+        modalEl.classList.remove("is-hidden");
+        document.body.classList.add('no-scroll');
+    }
+));
+
+
+addToListBtn.addEventListener("click", () => { 
+    const image = imageEl.src;
+    const title = titleEl.textContent;
+    const author = authorEl.textContent;
+    const description = descriptionEl.textContent;
+
+    const addToListData = {
+        image,
+        title,
+        author,
+        description,
+    }
+
+    localStorage.setItem('addtolistinfo', JSON.stringify(addToListData));
+    textSubmitEl.classList.remove("is-hidden");
+})
+
+          
+
+modalCloseBtn.addEventListener("click", () => {
+    modalEl.classList.add("is-hidden");
+    document.body.classList.remove('no-scroll');
+    localStorage.removeItem("bookinfo");
+    imageEl.src = "";
+    authorEl.textContent = "";
+    titleEl.textContent = "";
+    descriptionEl.textContent = "";
+    textSubmitEl.classList.add("is-hidden");
+});
+
 }
