f08b091878098e6079292c7fd54b840d5ea3f1df
vern-gora <103502360+vern-gora@users.noreply.github.com>
2023-07-05 12:48:06 +0300

    fix-1

--- src/index.js
+++ src/index.js
@@ -4,30 +4,30 @@
   | default import
   |============================
 */
-import './js/header.js';
-import './js/menu.js';
-import './js/support.js';
-import './js/support-items.js';
-import './js/support-img-array.js';
-import './js/books.js';
-import './js/categories.js';
-import './js/modal.js';
-import './js/shopping-list.js';
-import './js/book-card.js';
-import './js/button-scroll.js';
-import './js/footer.js';
-import './js/api.js';
-import './js/firebase/firebase-api.js';
-import './js/firebase/firebase-config.js';
-import { getUserData, signOut } from './js/firebase/firebase-api.js';
+// import './js/header.js';
+// import './js/menu.js';
+// import './js/support.js';
+// import './js/support-items.js';
+// import './js/support-img-array.js';
+// import './js/books.js';
+// import './js/categories.js';
+// import './js/modal.js';
+// import './js/shopping-list.js';
+// import './js/book-card.js';
+// import './js/button-scroll.js';
+// import './js/footer.js';
+// import './js/api.js';
+// import './js/firebase/firebase-api.js';
+// import './js/firebase/firebase-config.js';
+// import { getUserData, signOut } from './js/firebase/firebase-api.js';
 
-isUserSignIn();
+// isUserSignIn();
 
-async function isUserSignIn() {
-  const userId = localStorage.getItem('userId');
-  if (!userId) {
-    signOut();
-  } else {
-    getUserData();
-  }
-}
+// async function isUserSignIn() {
+//   const userId = localStorage.getItem('userId');
+//   if (!userId) {
+//     signOut();
+//   } else {
+//     getUserData();
+//   }
+// }
